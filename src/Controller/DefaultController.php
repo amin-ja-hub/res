8f2fff098b4981ec505f6770332a7863a3a3b331
Amin Jalili <amin.jalili.teh@gmail.com>
2024-08-14 16:21:40 +0330

    e

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends AbstractController
 {
@@ -16,16 +18,57 @@ class DefaultController extends AbstractController
         ]);
     }
 
-    #[Route('/products', name: 'products_shoe')]
-    public function Products(EntityManagerInterface $entityManager): Response
+    #[Route('/articles', name: 'article_show')]
+    public function Articles(EntityManagerInterface $entityManager, Request $request): Response
     {
-        $products = $entityManager
-            ->getRepository(\App\Entity\Product::class) // Fully qualified class name
-            ->findBy(['published' => 1]); // Find products where 'published' is 1
+        $page = $request->query->getInt('page', 1);
+        $limit = 1;
+        $offset = ($page - 1) * $limit;
 
-        return $this->render('default/front/product/list.html.twig', [
-            'products' => $products,
+        $query = $entityManager
+            ->getRepository(\App\Entity\Article::class)
+            ->createQueryBuilder('p')
+            ->where('p.published = :published')
+            ->setParameter('published', 1)
+            ->setFirstResult($offset)
+            ->setMaxResults($limit)
+            ->getQuery();
+
+        $paginator = new Paginator($query, $fetchJoinCollection = true);
+        $totalItems = count($paginator);
+        $totalPages = ceil($totalItems / $limit);
+
+        return $this->render('default/front/article/list.html.twig', [
+            'articles' => $paginator,
+            'totalPages' => $totalPages,
+            'currentPage' => $page,
         ]);
     }
 
+    #[Route('/products', name: 'product_show')]
+    public function Products(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $page = $request->query->getInt('page', 1);
+        $limit = 1;
+        $offset = ($page - 1) * $limit;
+
+        $query = $entityManager
+            ->getRepository(\App\Entity\Product::class)
+            ->createQueryBuilder('p')
+            ->where('p.published = :published')
+            ->setParameter('published', 1)
+            ->setFirstResult($offset)
+            ->setMaxResults($limit)
+            ->getQuery();
+
+        $paginator = new Paginator($query, $fetchJoinCollection = true);
+        $totalItems = count($paginator);
+        $totalPages = ceil($totalItems / $limit);
+
+        return $this->render('default/front/product/list.html.twig', [
+            'products' => $paginator,
+            'totalPages' => $totalPages,
+            'currentPage' => $page,
+        ]);
+    }
 }
